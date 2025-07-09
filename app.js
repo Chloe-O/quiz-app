6e6521ebad6d562e2e67c14f78552741d5a8953c
Chloe-O <chloeosbourne@yahoo.co.uk>
2025-07-09 21:44:47 +0100

    user feedback when correct/incorrect answer chosen

--- app.js
+++ app.js
@@ -2,13 +2,11 @@
 
 const questionObj = [
   {
-    ID: 1,
     question: "How many time zones are there in Russia?",
     answerOptions: [1, 11, 5, 10],
     correctAnswer: 11,
   },
   {
-    ID: 2,
     question: "What's the national flower of Japan?",
     answerOptions: ["Hibiscus", "Lotus flower", "Rose", "Cherry Blossom"],
     correctAnswer: "Cherry Blossom",
@@ -53,8 +51,8 @@ const questionObj = [
 
 const quizNavBtn = document.getElementById("qNavigationBtn");
 const answerBtnsContainer = document.getElementById("qOptions");
+const quizMessage = document.getElementById("quizMessage");
 const quizTitle = document.getElementById("qTitle");
-const questionID = document.getElementById("qID");
 const quizScore = document.getElementById("qScore");
 const optionBtns = document.querySelectorAll(".option-btn");
 
@@ -63,14 +61,55 @@ let questionIndex = 0;
 
 quizScore.innerText = score;
 
+function updateScore() {
+  score++;
+  quizScore.innerText = score;
+}
+
+function selectAnswer() {
+  let correct = questionObj[questionIndex].correctAnswer;
+  optionBtns.forEach((ans) => {
+    ans.addEventListener("click", (e) => {
+      console.log(correct);
+      if (e.target.innerText == correct) {
+        correctAnswerClicked(e);
+        updateScore();
+        resetQuestion(e);
+        iterateQuestions();
+      } else {
+        quizMessage.style.color = "red";
+        quizMessage.innerText = "Not quite! Try again";
+        // e.target.style.background = "red";
+      }
+    });
+  });
+}
+
+function correctAnswerClicked(e) {
+  quizMessage.style.color = "green";
+  quizMessage.innerText = "That's right!";
+  e.target.style.background = "green";
+  answerBtnsContainer.style.opacity = "0.5";
+  answerBtnsContainer.style.pointerEvents = "none";
+}
+
+// How to 'reset' question??
+function resetQuestion(e) {
+  quizMessage.style.color = "black";
+  quizMessage.innerText = "";
+  e.target.style.background = "#f0f0f0";
+  answerBtnsContainer.style.opacity = "1";
+  answerBtnsContainer.style.pointerEvents = "visible";
+}
+
 function iterateQuestions() {
   quizTitle.innerText = questionObj[questionIndex].question;
   let ansOptions = questionObj[questionIndex].answerOptions;
   optionBtns.forEach((x, index) => {
     x.innerText = ansOptions[index];
   });
-  questionIndex++;
   selectAnswer();
+  questionIndex++;
 }
 
 function startQuiz() {
@@ -78,23 +117,6 @@ function startQuiz() {
   quizNavBtn.classList.add("disabled");
   answerBtnsContainer.style.display = "block";
   iterateQuestions();
-}
-
-function selectAnswer() {
-  optionBtns.forEach((ans, index) => {
-    ans.addEventListener("click", (e) => {
-      let correctAnswer = questionObj[index].correctAnswer;
-      if (e.target.innerText == correctAnswer) {
-        console.log(
-          `Yay! You're correct, the right answer is ${correctAnswer}`
-        );
-      } else {
-        console.log(
-          `Not quite, ${e.target.innerText} is not the right answer!`
-        );
-      }
-    });
-  });
 }
 
 quizNavBtn.addEventListener("click", startQuiz);
